f7b4ce131240f17e3b960e48b072bb8eb4a5cf82
pycppdel <pycppdel@gmail.com>
2021-08-20 17:48:45 +0200

    Added Handles

---
+++ source/main.py
@@ -0,0 +1,7 @@
+"""
+Importing all important classes
+"""
+from terminal import TerminalHandler
+from sqhandler import SqHandler
+from external_text import *
+from variablecontainer import VariableContainer
---
+++ source/variablecontainer.py
@@ -0,0 +1,5 @@
+"""
+class for storing variables
+"""
+class VariableContainer:
+    pass
