b5c1804079958a9f21ac578abf77808d1614ae33
pycppdel <pycppdel@gmail.com>
2021-08-22 16:59:25 +0200

    Added Variable handler

--- source/variablecontainer.py
+++ source/variablecontainer.py
@@ -2,4 +2,36 @@
 class for storing variables
 """
 class VariableContainer:
-    pass
+
+    def __init__(self):
+        #container for variables
+        self.saved_variables = {}
+        #for changing variables
+        self.variable_operations = {
+
+        "string": str,
+        "int": int,
+        "byte": byte,
+        }
+
+    def exists(self, var):
+        """
+        checks if variable exists
+        """
+        return (var in self.variables.keys())
+
+    def get_var(self, name):
+        """
+        gives back value of var
+        """
+        return self.saved_variables[name]
+
+    def make_var(self, name, value):
+        """
+        makes variable
+        """
+        try:
+            self.saved_variables[name] = self.variable_operations[value]
+            return True
+        except Exception:
+            return False
